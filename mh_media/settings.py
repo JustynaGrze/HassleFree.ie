db245e0fc12409a9af5b755b6f72a74d81de112b
Justyna Grzeszczyk <justyna.grzeszczyk91@gmail.com>
2019-06-30 20:19:03 +0000

    set up heroku envirnovars

--- mh_media/settings.py
+++ mh_media/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wfr56(p-7@wue!p5w2hp5s$oq(pa_^u04qst)_p1fu-z(mq+dv'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get('DEBUG_VALUE') == "True":
+    DEBUG = True
+else:
+    DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'hasslefree.herokuapp.com']
 
 
 # Application definition
